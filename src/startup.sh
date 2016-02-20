72ad3a1043be13f55f81a95eb0f3319e8f026d98
Chris Straffon <muppet_3000@hotmail.com>
2016-02-20 21:37:49 +0000

    Removed the old startup script

--- src/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Running App"
-cd /opt/eagleowl
-/opt/eagleowl/cm160 &> /var/log/cm160.log &
-echo "Sleeping"
-sleep 20
-echo "Done sleeping - starting app"
-python /opt/rpi-eco-light/application.py &> /var/log/rpi-eco-light.log &
